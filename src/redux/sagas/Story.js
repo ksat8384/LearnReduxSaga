dee26d400cd2a78b3258ff1ae9fb712acef2e201
ksat8384 <ksat8384@gmail.com>
2022-05-09 17:19:11 +0530

    separated api logic and sagas

--- src/redux/sagas/Story.js
+++ src/redux/sagas/Story.js
@@ -1,15 +1,11 @@
 import { call, put } from "redux-saga/effects";
 import { addStories } from "../slices/StoriesSlice";
+import { fetchStories } from "../../api/story";
 
-const HN_BASE_URL = "http://hn.algolia.com/api/v1/search?query=";
+function* handleFetchStories(action) {
+  const { query } = action;
+  const result = yield call(fetchStories, query);
+  yield put(addStories(result.hits));
+}
 
-const fetchStories = (query) =>
-    fetch(HN_BASE_URL + query).then((response) => response.json());
-  
-function* handleFetchStories(action) { 
-    const { query } = action;
-    const result = yield call(fetchStories, query)
-    yield put(addStories(result.hits))
-}   
-
-export { handleFetchStories }
+export { handleFetchStories };
